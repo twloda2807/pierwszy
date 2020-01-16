fa8b5e934163c81a1e59cb32b82d2c8bbce4765f
twloda2807 <59970604+twloda2807@users.noreply.github.com>
2020-01-16 18:19:34 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-
+require (["esri/Map","esri/views/MapView"],
+    function(Map, MapView){
+        let map1 = new Map({basemap: "topo"});
+        let mapContainer = new MapView({
+            container: "item",
+            map: map1,
+            zoom: 10,
+            center: [22.66,51.25]
+        })
+    })
